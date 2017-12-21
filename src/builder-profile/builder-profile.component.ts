dcdf03f272a0dc22876da7fac098a2b429b83853
rriryk <rriryk@gmail.com>
2017-12-21 17:45:07 +0200

    Builder profile summary change border color on blur

--- src/builder-profile/builder-profile.component.ts
+++ src/builder-profile/builder-profile.component.ts
@@ -7,9 +7,21 @@ import { Store } from '@ngrx/store';
     styleUrls: ['./builder-profile.component.scss']
 })
 export class BuilderProfileComponent implements OnInit {
+    private isInEditMode: boolean;
+
     constructor(private store: Store<any>) {
     }
 
     public ngOnInit() {
+    }
+    
+    builderProfileOnFocus(event: any) {
+    	console.log("EditMode");
+        this.isInEditMode = true;  
+    }
+
+    builderProfileOnFocusout(event: any) {
+    	console.log("ViewMode");
+        this.isInEditMode = false;       
     }
 }
